5507941f7b067bd1e08e6ade2b03eae82b97fdc1
Matthew Furseman <matthew.furseman@ukaea.uk>
2022-03-10 07:03:57 +0000

    Large refactor

--- gearing.py
+++ gearing.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python3.10
+
+import bisect
 import itertools
-import sortedcontainers
+import math
+
 
 INCH_TO_MM = 25.4
-PRECISON = 6 # round to one nanometer per cycle pitch error
+
+
+TARGET_ERROR = 1.0 / 1000.0
+PRINT_FORMAT = "0.3f"
 
 
 class GearSet:
-    def __init__(self, gears):
-        self.gears = sorted(gears)
-        self.ratio = 1
-        for pair in list(gears):
-            self.ratio *= pair[1] / pair[0]
+    def __init__(self, driving, driven):
+        assert len(driving) == len(driven)
+        self.driving = sorted(driving)
+        self.driven = sorted(driven)
+        self.ratio = math.prod(self.driven) / math.prod(self.driving)
 
     def __eq__(self, other: object) -> bool:
         return self.ratio == other.ratio
@@ -19,45 +26,50 @@ class GearSet:
         return self.ratio < other.ratio
 
     def __hash__(self) -> int:
-        return hash(tuple(self.gears))
+        return hash(tuple((tuple(self.driving), tuple(self.driven))))
 
     def __repr__(self) -> str:
-        return f'{self.ratio:0.6f} -> [{list(self.gears)}]'
-
-
-def closest_ratio(ratio, ratio_groups) -> list:
-    values = ratio_groups.values()
-    x = list(values)
-    idx = ratio_groups.bisect_left(ratio)
-    if idx == 0:
-        return values[0]
-    if idx == len(ratio_groups):
-        return values[-1]
-    lower, upper = ratio_groups.keys()[idx - 1:idx + 1]
-    if abs(ratio - lower) < abs(ratio - upper):
-        return values[idx - 1]
-    return values[idx]
+        return f"{self.ratio:^{PRINT_FORMAT}} -> [{list(self.driving)} : {list(self.driven)}]"
 
 
 def pick_best(gear_set, preferred_drive_gear, preferred_driven_gear) -> list:
-    shortest_train_length = min([len(gs.gears) for gs in gear_set])
-    shortest_trains = [gs for gs in gear_set if len(gs.gears) == shortest_train_length]
-
-    preffered_smallest = {st for st in shortest_trains if any(True for s in st.gears if s[0] == preferred_drive_gear)}
-    preffered_largest = {st for st in shortest_trains if any(True for s in st.gears if s[1] == preferred_driven_gear)}
+    shortest_train_length = min([len(gs.driving) for gs in gear_set])
+    shortest_trains = {
+        gs for gs in gear_set if len(gs.driving) == shortest_train_length
+    }
+    preffered_smallest = {
+        st
+        for st in shortest_trains
+        if any(True for s in st.driving if s == preferred_drive_gear)
+    }
+    preffered_largest = {
+        st
+        for st in shortest_trains
+        if any(True for s in st.driven if s == preferred_driven_gear)
+    }
     if preffered_smallest & preffered_largest:
-        return preffered_smallest & preffered_largest
+        return (preffered_smallest & preffered_largest).pop()
     elif preffered_smallest:
-        return preffered_smallest
+        return preffered_smallest.pop()
     elif preffered_largest:
-        return preffered_largest
-    return shortest_trains
+        return preffered_largest.pop()
+    return shortest_trains.pop()
 
 
-gears = sorted([20, 20, 22, 26, 28, 30, 32, 38, 45, 50, 50, 55, 73])
+def filter_best(gear_set):
+    shortest_train_length = min([len(gs.driving) for gs in gear_set])
+    shortest_trains = {
+        gs for gs in gear_set if len(gs.driving) == shortest_train_length
+    }
+    return list(shortest_trains)
+
+
+# gears = sorted([20, 20, 30, 30, 35, 38, 40, 45, 46, 50, 55, 60, 65, 73])
+gears = sorted([20, 20, 22, 28, 30, 32, 38, 38, 40, 45, 50, 50, 55, 73])
+gears = sorted([20, 20, 22, 28, 30, 32, 38, 38, 40, 50, 50, 55, 73])
+
 
 metric_pitches = [  # mm
-    0.10,
     0.15,
     0.20,
     0.25,
@@ -86,6 +98,11 @@ metric_pitches = [  # mm
 ]
 
 imperial_pitches = [  # tpi
+    180,
+    160,
+    140,
+    120,
+    100,
     80,
     72,
     64,
@@ -96,11 +113,14 @@ imperial_pitches = [  # tpi
     36,
     32,
     28,
+    26,
     24,
     20,
+    19,
     18,
     16,
     14,
+    13,
     12,
     11,
     10,
@@ -120,29 +140,81 @@ imperial_pitches = [  # tpi
 ]
 
 
-permutations = list(itertools.permutations(gears, 4)) + list(itertools.permutations(gears, 6))
-gearsets = sorted({GearSet(x) for x in {tuple(zip(p[::2], p[1::2])) for p in permutations}})
+gear_sets = []
+combinations = list(set([c for c in itertools.combinations(gears, 6)]))
+for combination in combinations:
+    unpaired = list(itertools.combinations(combination, 3))
+    halflength = len(unpaired)
+    paired = zip(unpaired, unpaired[::-1])
+    for pair in paired:
+        gear_sets.append(GearSet(pair[0], pair[1]))
 
-ratio_groups = sortedcontainers.SortedDict()
-for gs in gearsets:
-    ratio_groups.setdefault(gs.ratio, []).append(gs)
+combinations = list(set([c for c in itertools.combinations(gears, 4)]))
+for combination in combinations:
+    unpaired = list(itertools.combinations(combination, 2))
+    halflength = len(unpaired)
+    paired = zip(unpaired, unpaired[::-1])
+    for pair in paired:
+        gear_sets.append(GearSet(pair[0], pair[1]))
+
+combinations = list(set([c for c in itertools.combinations(gears, 2)]))
+for combination in combinations:
+    unpaired = list(itertools.combinations(combination, 1))
+    halflength = len(unpaired)
+    paired = zip(unpaired, unpaired[::-1])
+    for pair in paired:
+        gear_sets.append(GearSet(pair[0], pair[1]))
+
+
+gear_sets = sorted(gear_sets)
+
 
 starting_pitch = 1 / 8  # 8 tpi leadscrew in inches per thread
 
 print()
-print('Metric Pitches')
-print('-' * 120)
-print('  mm \t actual \t\t Gearsets')
+print("Metric Pitches")
+print("-" * 120)
+print("  mm \t ratio \t\t Gearsets")
 for pitch in metric_pitches:
     ratio = INCH_TO_MM * starting_pitch / pitch
-    best = pick_best(closest_ratio(ratio, ratio_groups), gears[1], gears[-1])
-    print(f'{pitch:0.3f}\t({INCH_TO_MM * starting_pitch / next(iter(best)).ratio:0.3f}) \t->\t{[b.gears for b in best]}')
+    left = bisect.bisect_left(
+        gear_sets,
+        ratio * (1 - TARGET_ERROR),
+        hi=len(gear_sets) - 1,
+        key=lambda gs: gs.ratio,
+    )
+    right = (
+        bisect.bisect_right(
+            gear_sets,
+            ratio * (1 + TARGET_ERROR),
+            hi=len(gear_sets) - 1,
+            key=lambda gs: gs.ratio,
+        )
+        + 1
+    )
+    gear_set = filter_best(gear_sets[left:right])
+    print(f"{pitch:^{PRINT_FORMAT}}\t({ratio:^{PRINT_FORMAT}}) \t->\t{gear_set}")
 
 print()
-print('Imperial Pitchs')
-print('-' * 120)
-print('  tpi \t actual \t\t GearSets')
+print("Imperial Pitchs")
+print("-" * 120)
+print("  tpi \t ratio \t\t GearSets")
 for pitch in imperial_pitches:
     ratio = starting_pitch * pitch
-    best = pick_best(closest_ratio(ratio, ratio_groups), gears[1], gears[-1])
-    print(f'{pitch:0.3f}\t({next(iter(best)).ratio / starting_pitch:0.3f}) \t->\t{[b.gears for b in best]}')
+    left = bisect.bisect_left(
+        gear_sets,
+        ratio * (1 - TARGET_ERROR),
+        hi=len(gear_sets) - 1,
+        key=lambda gs: gs.ratio,
+    )
+    right = (
+        bisect.bisect_right(
+            gear_sets,
+            ratio * (1 + TARGET_ERROR),
+            hi=len(gear_sets) - 1,
+            key=lambda gs: gs.ratio,
+        )
+        + 1
+    )
+    gear_set = filter_best(gear_sets[left:right])
+    print(f"{pitch:^{PRINT_FORMAT}}\t({ratio:^{PRINT_FORMAT}}) \t->\t{gear_set}")
